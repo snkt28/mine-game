edb753e9b4780b2756947179c2bf4ad12c999495
“snkt28” <sanket28006@gmail.com>
2025-09-07 11:49:59 +0530

    footer added and top nav logo

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -15,7 +15,7 @@ export default function Grid(){
     return (
         <>
         <div className='Grid'>
-            <div className="Topnav"><div className='money'><h3><i className="fa-solid fa-sack-dollar"></i>Balance is : {money.toFixed(2)}</h3></div>            </div>
+            <div className="Topnav"><div className='money'><h2 className='logo'>MINE GAME</h2><h3><i className="fa-solid fa-sack-dollar"></i>Balance is : {money.toFixed(2)}</h3></div>            </div>
             <div className="Game" ><Game updategems={gemClick} /></div>
             <div className="Sidenav" ><Sidenav gemclicked={gem}/></div>
         </div>
